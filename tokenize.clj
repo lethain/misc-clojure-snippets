0de66df5f52ff8917f0d3d6b786a10136306d867
Will Larson <lethain@will-larsons-macbook.local>
2009-11-15 15:12:30 -0800

    Reduced to 4 lines.. ;)

--- tokenize.clj
+++ tokenize.clj
@@ -1,18 +1,4 @@
-(ns my-ns 
-  (:import (java.io BufferedReader FileReader)))
-
-(defn process-line [acc line]
-  (println line)
-  (println acc)
-  (+ 1 (int acc)))
-
-(defn process-lines [file-name]
-  (with-open [rdr (BufferedReader. (FileReader. file-name))]
-					; (reduce (fn [n y] (+ n (:fred y))) 0 x)
-    (reduce process-line (int 0) (line-seq rdr))))
-
-(process-lines "tokenize.clj")
-
-
-
-
+(ns my-ns (:use [clojure.contrib.duck-streams :only (spit read-lines)]))
+(prn (reduce (fn [acc line] (reduce #(assoc %1 %2 (+ (get %1 %2 0) 1))
+			       acc (.split (.toLowerCase line) " ")))
+	(hash-map) (read-lines (first (rest *command-line-args*)))))
